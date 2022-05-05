9161712255d75acfdcf9b445e544ed1d1f26a1d7
kathrynzaro <zarokathryn@gmail.com>
2022-05-05 10:49:04 -0700

    created books.js / created array of three books incl. title, author, and year / books div in html

---
+++ books.js
@@ -0,0 +1,5 @@
+export const books = [
+    { title: 'Blood Meridian', author: 'Cormac McCarthy', yearPublished: 1985 },
+    { title: 'An American Marriage', author: 'Tayari Jones', yearPublished: 2018 },
+    { title: 'The Picture of Dorian Gray', author: 'Oscar Wilde', yearPublished: 1890 },
+]
