ebcd24266733e9791cafe8224623dd2184013e06
kathrynzaro <zarokathryn@gmail.com>
2022-05-06 10:24:30 -0700

    added roadtrip.js and html

---
+++ roadtrip.js
@@ -0,0 +1,27 @@
+export const stops = [
+    {
+        stop: 'Chapel in the Hills', 
+        image: './assets/chapel.jpg',
+        where: 'South Dakota', 
+        why: 'attempt to get married',
+    },
+    { 
+        stop: 'Rosehill Cemetery',
+        image: './assets/rosehill.jpg',
+        where: 'Chicago, IL',
+        who: ['Kat', 'Joe', 'Renly'],
+        why: 'go for a stroll'
+    },
+    {
+        stop: 'A Rest Stop',
+        image: './assets/joe.jpg',
+        where: 'Montana',
+        why: 'take this photo primarily'
+    },
+    {
+        stop: 'A Hotel',
+        image: './assets/brian.jpg',
+        where: 'New Jersey',
+        why: "visit Joe's brother Brian (who we're trying to convince to go to Alchemy!",
+    },
+];
